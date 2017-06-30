bfd1e71a004505f19e44ccad56042dedf0d98c25
jja <gitter@tutanota.com>
2017-06-30 17:58:03 -0500

    Establece entorno, crea proyecto y aplicación

---
+++ entorno/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jja/Documents/edu/desarrollo/devf/challenges/semana2/hackernews/entorno/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
